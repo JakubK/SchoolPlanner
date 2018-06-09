3287dda8a098d5fe0f3d6bebdf9660c043998d61
DESKTOP-NJF0PAT\jakub <jakubkuniszewski1@gmail.com>
2018-06-09 15:33:42 +0200

    Create BaseViewModel and it's implementation MainViewModel and set MainViewModel as the DataContext for MainWindow.xaml

---
+++ SchoolPlanner/ViewModels/MainViewModel.cs
@@ -0,0 +1,19 @@
+﻿using SchoolPlanner.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SchoolPlanner.ViewModels
+{
+    public class MainViewModel : BaseViewModel
+    {
+        public Window Window;
+        public MainViewModel(Window w)
+        {
+            this.Window = w;
+        }
+    }
+}
