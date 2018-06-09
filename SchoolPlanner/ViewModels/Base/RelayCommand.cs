75dba07f02a4fce627484f0fc624f172b9ead356
DESKTOP-NJF0PAT\jakub <jakubkuniszewski1@gmail.com>
2018-06-09 15:50:42 +0200

    Create RelayCommand for executing Commands and use it for Minimize, Maximize and Close Buttons in the MainViewModel

---
+++ SchoolPlanner/ViewModels/Base/RelayCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SchoolPlanner.ViewModels.Base
+{
+    public class RelayCommand : ICommand
+    {
+        private Action Action;
+        public event EventHandler CanExecuteChanged;
+
+        public RelayCommand(Action action)
+        {
+            this.Action = action;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            Action();
+        }
+    }
+}
--- SchoolPlanner/ViewModels/MainViewModel.cs
+++ SchoolPlanner/ViewModels/MainViewModel.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Input;
 
 namespace SchoolPlanner.ViewModels
 {
@@ -14,6 +15,14 @@ namespace SchoolPlanner.ViewModels
         public MainViewModel(Window w)
         {
             this.Window = w;
+
+            this.CloseCommand = new RelayCommand(() => Window.Close());
+            this.MaximizeCommand = new RelayCommand(() => Window.WindowState = WindowState.Maximized);
+            this.MinimizeCommand = new RelayCommand(() => Window.WindowState = WindowState.Minimized);
         }
+
+        public ICommand CloseCommand { get; set; }
+        public ICommand MaximizeCommand { get; set; }
+        public ICommand MinimizeCommand { get; set; }
     }
 }
