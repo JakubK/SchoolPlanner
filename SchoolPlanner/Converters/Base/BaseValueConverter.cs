8914e0c4e14acfffc04eac22f9c3867e2c8d9357
DESKTOP-NJF0PAT\jakub <jakubkuniszewski1@gmail.com>
2018-06-11 19:25:52 +0200

    Add BaseValueConverter abstract class for singleton ValueConverters and it's first implementation BoolToVisibilityConverter

---
+++ SchoolPlanner/Converters/Base/BaseValueConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace SchoolPlanner.Converters
+{
+    public abstract class BaseValueConverter<T> : MarkupExtension, IValueConverter where T : class, new()
+    {
+        public abstract object Convert(object value, Type targetType, object parameter, CultureInfo culture);
+        public abstract object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture);
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return Converter ?? (Converter = new T());
+        }
+
+        private static T Converter = null;
+    }
+}
---
+++ SchoolPlanner/Converters/BoolToVisibilityConverter.cs
@@ -0,0 +1,26 @@
+﻿using SchoolPlanner.Converters;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace SchoolPlanner
+{
+    public class BoolToVisibilityConverter : BaseValueConverter<BoolToVisibilityConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool isVisible = (bool)value;
+            return isVisible ? Visibility.Visible : Visibility.Hidden;
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
