697a4e30065f8f32eeab8f81aa11cc498e61795a
DESKTOP-NJF0PAT\jakub <jakubkuniszewski1@gmail.com>
2018-06-11 19:28:54 +0200

    Add PlansListViewViewModel which has a command for toggling the List itself

---
+++ SchoolPlanner/ViewModels/PlansListViewViewModel.cs
@@ -0,0 +1,23 @@
+﻿using SchoolPlanner.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SchoolPlanner.ViewModels
+{
+    public class PlansListViewViewModel : BaseViewModel
+    {
+        private PlansListView PlansListView;
+        public PlansListViewViewModel(PlansListView plansListView)
+        {
+            this.PlansListView = plansListView;
+
+            SwitchItemsCommand = new RelayCommand(() => plansListView.ListHeight = plansListView.ListHeight == 200 ? 0 : 200);
+        }
+
+        public ICommand SwitchItemsCommand { get; set; }
+    }
+}
