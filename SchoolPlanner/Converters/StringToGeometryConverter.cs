bbba72b4babac064d66550ae3ec4dbb7ef10fc02
DESKTOP-NJF0PAT\jakub <jakubkuniszewski1@gmail.com>
2018-06-12 15:58:38 +0200

    Add StringToGeometryConverter

---
+++ SchoolPlanner/Converters/StringToGeometryConverter.cs
@@ -0,0 +1,24 @@
+﻿using SchoolPlanner.Converters;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace SchoolPlanner
+{
+    public class StringToGeometryConverter : BaseValueConverter<StringToGeometryConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Geometry.Parse((string)value);
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
