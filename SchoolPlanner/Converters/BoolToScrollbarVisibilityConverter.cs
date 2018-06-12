9672d286092607787778063048ef46c6a3aeefc6
DESKTOP-NJF0PAT\jakub <jakubkuniszewski1@gmail.com>
2018-06-12 16:23:59 +0200

    Add BoolToScrollbarVisibilityConverter

---
+++ SchoolPlanner/Converters/BoolToScrollbarVisibilityConverter.cs
@@ -0,0 +1,25 @@
+﻿using SchoolPlanner.Converters;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace SchoolPlanner
+{
+    public class BoolToScrollbarVisibilityConverter : BaseValueConverter<BoolToScrollbarVisibilityConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool val = (bool)value;
+            return val == true ? ScrollBarVisibility.Visible : ScrollBarVisibility.Hidden;
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
