b09af9c769f44c0c4bc88c537e88690fc5153d0f
DESKTOP-NJF0PAT\jakub <jakubkuniszewski1@gmail.com>
2018-06-14 20:44:57 +0200

    Create new Page "GridPage" and attach it to the Frame in the MainWindow.xaml

---
+++ SchoolPlanner/ViewModels/GridPageViewModel.cs
@@ -0,0 +1,19 @@
+﻿using SchoolPlanner.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolPlanner.ViewModels
+{
+    public class GridPageViewModel : BaseViewModel
+    {
+        public GridPageViewModel()
+        {
+            Message = "Hello";
+        }
+
+        public string Message { get; set; }
+    }
+}
