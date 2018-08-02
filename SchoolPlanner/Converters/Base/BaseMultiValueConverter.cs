ca40e42160d37c7a20c464f84e286da8cb7d7d8d
DESKTOP-NJF0PAT\jakub <jakubkuniszewski1@gmail.com>
2018-08-02 19:53:01 +0200

    Add BaseMultiValueConverter and IntsToCellRequestConverter

---
+++ SchoolPlanner/Converters/Base/BaseMultiValueConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace SchoolPlanner.Converters
+{
+    public abstract class BaseMultiValueConverter<T> : MarkupExtension, IMultiValueConverter where T : class,new()
+    {
+        public abstract object Convert(object[] values, Type targetType, object parameter, CultureInfo culture);
+
+        public abstract object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture);
+
+        private static T Coverter = null;
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return Coverter ?? (Coverter = new T());
+        }
+    }
+}
---
+++ SchoolPlanner/Converters/IntsToCellRequestConverter.cs
@@ -0,0 +1,28 @@
+﻿using SchoolPlanner.Converters;
+using SchoolPlanner.Models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolPlanner
+{
+    public class IntsToCellRequestConverter : BaseMultiValueConverter<IntsToCellRequestConverter>
+    {
+        public override object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            return new CellRequest
+            {
+                X = (int)values[0],
+                Y = (int)values[1]
+            };
+        }
+
+        public override object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
