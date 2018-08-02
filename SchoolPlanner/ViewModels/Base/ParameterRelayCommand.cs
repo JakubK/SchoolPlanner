580c566f897a381677ca3fd2499a22a6c748129f
DESKTOP-NJF0PAT\jakub <jakubkuniszewski1@gmail.com>
2018-08-02 19:54:08 +0200

    Add ParamterRelayCommand which is just a RelayCommand that can be passed with an extra paramter

---
+++ SchoolPlanner/ViewModels/Base/ParameterRelayCommand.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SchoolPlanner.ViewModels.Base
+{
+    public class ParameterRelayCommand : ICommand
+    {
+        private Action<object> mAction;
+
+        public ParameterRelayCommand(Action<object> action)
+        {
+            mAction = action;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            mAction(parameter);
+        }
+    }
+}
