22d6350e61451471270f794c0802a42257e0c2dd
DESKTOP-NJF0PAT\jakub <jakubkuniszewski1@gmail.com>
2018-08-25 22:32:21 +0200

    Handle cell click depending on it's CellType

--- SchoolPlanner/ViewModels/Pages/PlanPageViewModel.cs
+++ SchoolPlanner/ViewModels/Pages/PlanPageViewModel.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using System.Windows.Media;
 
 namespace SchoolPlanner.ViewModels
 {
@@ -17,11 +18,75 @@ namespace SchoolPlanner.ViewModels
         private PlanPageViewModel()
         {
             RemoveCellCommand = new ParameterRelayCommand(id => RemoveCell(id));
+
+            CellClickCommand = new ParameterRelayCommand(query => CellClick(query));
         }
 
         private void RemoveCell(object id)
         {
 
+        }
+
+        private void CellClick(object query)
+        {
+            CellRequest cellRequest = (CellRequest)query;
+            if (cellRequest.CellType == CellType.RowAppend)
+            {
+                int y = cellRequest.Y;
+                Plan.AddRow.Y++;
+                Plan.AddColumn.SpanY++;
+
+                for(int i = 1;i < Plan.AddRow.SpanX;i++)
+                {
+                    Plan.Cells.Add(new CellViewModel
+                    {
+                        Text = "Text",
+                        Background = Brushes.Violet,
+                        X = i,
+                        Y = y,
+                        SpanY = 1
+                    });
+                }
+
+                Plan.Cells.Add(new CellViewModel
+                {
+                    Text = "Remove Column",
+                    Background = Brushes.Blue,
+                    X = 0,
+                    Y = y,
+                    SpanX = 1,
+                    SpanY = 1
+                });
+
+            }
+            else if(cellRequest.CellType == CellType.ColumnAppend)
+            {
+                int x = cellRequest.X;
+                Plan.AddColumn.X++;
+                Plan.AddRow.SpanX++;
+                Plan.AddRow.SpanY++;
+                for (int i = 1; i < Plan.AddColumn.SpanY; i++)
+                {
+                    Plan.Cells.Add(new CellViewModel
+                    {
+                        Text = "Text",
+                        Background = Brushes.Violet,
+                        X = x,
+                        Y = i,
+                        SpanY = 1
+                    });
+                }
+
+                Plan.Cells.Add(new CellViewModel
+                {
+                    Text = "Remove Column",
+                    Background = Brushes.Blue,
+                    X = x,
+                    Y = 0,
+                    SpanX = 1,
+                    SpanY = 1
+                });
+            }
         }
 
         private Plan plan;
